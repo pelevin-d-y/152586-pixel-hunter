e15cedc976b537e117bc8d636912b10e9ba0de0e
natpuot92 <natpuot1992@gmail.com>
2017-09-28 19:55:50 +0300

    fix2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-"use strict";
-
 (function () {
   const mainElement = document.querySelector(`.central`);
-  const KEY_CODE_ALT = `18`;
-  const KEY_CODE_LEFT = `37`;
-  const KEY_CODE_RIGHT = `39`;
+  const KEY_CODE_LEFT = 37;
+  const KEY_CODE_RIGHT = 39;
   let templateNumber = -1;
 
   const arrayTemplatesId = [`#greeting`, `#rules`, `#game-1`, `#game-2`, `#game-3`, `#stats`];
-  const arrayTemplates = arrayTemplatesId.map(function (id) {
-    return document.querySelector(id);
-  });
+  const arrayTemplates = arrayTemplatesId.map((id) => document.querySelector(id));
 
   const showWindow = function (number) {
     const newElement = arrayTemplates[number].content.cloneNode(true);
@@ -25,34 +20,23 @@
     mainElement.appendChild(newElement);
   };
 
-  const pressed = {};
-
   document.addEventListener(`keydown`, function (evt) {
-
-    pressed[evt.keyCode] = true;
-    const keysPressed = Object.keys(pressed);
-
-    if (keysPressed[0] === KEY_CODE_ALT && keysPressed[1] === KEY_CODE_RIGHT) {
-      if (templateNumber === -1) {
-        templateNumber = 0;
+    if (evt.altKey && evt.keyCode === KEY_CODE_RIGHT) {
+      if (templateNumber >= -1 && templateNumber < arrayTemplates.length - 1) {
+        templateNumber++;
         showWindow(templateNumber);
-      } else if (templateNumber < arrayTemplates.length - 1) {
-        showWindow(templateNumber + 1);
-        templateNumber = templateNumber + 1;
       }
     }
 
-    if (keysPressed[0] === KEY_CODE_ALT && keysPressed[1] === KEY_CODE_LEFT) {
+    if (evt.altKey && evt.keyCode === KEY_CODE_LEFT) {
       if (templateNumber === -1) {
-        templateNumber = 0;
-      } else if (templateNumber > 0) {
-        showWindow(templateNumber - 1);
+        return;
+      }
+
+      if (templateNumber > 0) {
         templateNumber--;
+        showWindow(templateNumber);
       }
     }
-  });
-
-  document.addEventListener(`keyup`, function (evt) {
-    delete pressed[evt.keyCode];
   });
 })();
