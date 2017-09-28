a42e7cbbd023040f70c3219e7cb08dbf59af2a35
natpuot92 <natpuot1992@gmail.com>
2017-09-28 17:58:37 +0300

    fix1

--- js/main.js
+++ js/main.js
@@ -2,31 +2,26 @@
 
 (function () {
   const mainElement = document.querySelector(`.central`);
-  const greetingTemplate = document.querySelector(`#greeting`);
-  const rulesTemplate = document.querySelector(`#rules`);
-  const gameOneTemplate = document.querySelector(`#game-1`);
-  const gameTwoTemplate = document.querySelector(`#game-2`);
-  const gameThreeTemplate = document.querySelector(`#game-3`);
-  const statsTemplate = document.querySelector(`#stats`);
   const KEY_CODE_ALT = `18`;
   const KEY_CODE_LEFT = `37`;
   const KEY_CODE_RIGHT = `39`;
   let templateNumber = -1;
 
-  const arrayTemplatesId = [`#greeting`, `#rules`, `#game-1`, `#game-2`, `#game-3`, `#stats`]
-  const arrayTemplateElements = arrayTemplatesId.map(function (id) {
+  const arrayTemplatesId = [`#greeting`, `#rules`, `#game-1`, `#game-2`, `#game-3`, `#stats`];
+  const arrayTemplates = arrayTemplatesId.map(function (id) {
     return document.querySelector(id);
   });
-
-  console.log(arrayTemplateElements);
-  const arrayTemplates = [greetingTemplate, rulesTemplate, gameOneTemplate, gameTwoTemplate, gameThreeTemplate, statsTemplate];
 
   const showWindow = function (number) {
     const newElement = arrayTemplates[number].content.cloneNode(true);
 
-    for (let i = mainElement.children.length - 1; i >= 0; i--) {
-      mainElement.children[i].remove();
-    }
+    const mainChildrens = mainElement.children;
+    const mainChildrensArray = [].slice.call(mainChildrens);
+
+    mainChildrensArray.forEach(function (element) {
+      element.remove();
+    });
+
     mainElement.appendChild(newElement);
   };
 
@@ -35,32 +30,29 @@
   document.addEventListener(`keydown`, function (evt) {
 
     pressed[evt.keyCode] = true;
-
     const keysPressed = Object.keys(pressed);
 
     if (keysPressed[0] === KEY_CODE_ALT && keysPressed[1] === KEY_CODE_RIGHT) {
       if (templateNumber === -1) {
         templateNumber = 0;
         showWindow(templateNumber);
-      } else if (templateNumber === arrayTemplates.length - 1) {
-        return;
-      } else if (templateNumber >= 0) {
+      } else if (templateNumber < arrayTemplates.length - 1) {
         showWindow(templateNumber + 1);
         templateNumber = templateNumber + 1;
       }
     }
 
     if (keysPressed[0] === KEY_CODE_ALT && keysPressed[1] === KEY_CODE_LEFT) {
-      if (templateNumber === -1 || templateNumber === 0) {
+      if (templateNumber === -1) {
         templateNumber = 0;
       } else if (templateNumber > 0) {
         showWindow(templateNumber - 1);
         templateNumber--;
       }
     }
+  });
 
-    document.onkeyup = function () {
-      delete pressed[evt.keyCode];
-    };
+  document.addEventListener(`keyup`, function (evt) {
+    delete pressed[evt.keyCode];
   });
 })();
